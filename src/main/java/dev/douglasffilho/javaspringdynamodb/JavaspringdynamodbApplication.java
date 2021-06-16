52692fe015873f12726a41606876ea70c7a19234
Douglas Fernandes <douglasf.filho@gmail.com>
2021-06-16 17:42:28 -0300

    build: add project base

---
+++ src/main/java/dev/douglasffilho/javaspringdynamodb/JavaspringdynamodbApplication.java
@@ -0,0 +1,13 @@
+package dev.douglasffilho.javaspringdynamodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaspringdynamodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaspringdynamodbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/douglasffilho/javaspringdynamodb/JavaspringdynamodbApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.douglasffilho.javaspringdynamodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaspringdynamodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
